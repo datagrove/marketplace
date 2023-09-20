e9b6d9bde99feafa7514f334347a0ec92ad99917
germanzarkovich <germanzarkovich@gmail.com>
2023-09-20 15:13:20 -0400

    add tinymce as a it own component

--- src/components/posts/CreateNewPost.tsx
+++ src/components/posts/CreateNewPost.tsx
@@ -26,6 +26,7 @@ import { icons } from "../../../node_modules/tinymce/icons/default/icons";
 import lists from "../../../node_modules/tinymce/plugins/lists/plugin";
 //@ts-ignore
 import quickbars from "../../../node_modules/tinymce/plugins/quickbars/plugin";
+import { TinyMCE } from "./TinyMCE";
 
 const lang = getLangFromUrl(new URL(window.location.href));
 const t = useTranslations(lang);
@@ -52,12 +53,12 @@ export const CreateNewPost: Component = () => {
   const [formData, setFormData] = createSignal<FormData>();
   const [response] = createResource(formData, postFormData);
   const [imageUrl, setImageUrl] = createSignal<Array<string>>([]);
-  const [mode, setMode] = createSignal<"dark" | "light">(
-    //@ts-ignore
-    localStorage.getItem("theme")
-  );
-
-  console.log("Start Mode: " + mode());
+  // const [mode, setMode] = createSignal<"dark" | "light">(
+  //   //@ts-ignore
+  //   localStorage.getItem("theme")
+  // );
+  //
+  // console.log("Start Mode: " + mode());
 
   // onMount(() => {
   //   window.addEventListener("storage", (event) => {
@@ -69,67 +70,67 @@ export const CreateNewPost: Component = () => {
   //   console.log(mode());
   // })
 
-  createEffect(() => {
-    window.addEventListener("storage", (event) => {
-      if (event.key === "theme") {
-        //@ts-ignore
-        setMode(event.newValue);
-      }
-      window.location.reload();
-      console.log(event.key);
-      console.log("Mode: " + mode());
-    });
-  });
-
-  createEffect(() => {
-    const script = document.createElement("script");
-    script.src = "/tinymce/tinymce.min.js";
-    script.async = true;
-    script.onload = () => {
-      const listPlugin = document.createElement("script");
-      listPlugin.src = "/tinymce/plugins/lists/plugin.min.js";
-      listPlugin.async = true;
-      listPlugin.onload = () => {
-        const quickBarsPlugin = document.createElement("script");
-        quickBarsPlugin.src = "/tinymce/plugins/quickbars/plugin.min.js";
-        quickBarsPlugin.async = true;
-        quickBarsPlugin.onload = () => {
-          console.log("tinymce loaded");
-          tinymce.init({
-            selector: "#Content",
-            max_width: 384,
-            skin_url:
-              mode() === "dark"
-                ? "/tinymce/skins/ui/oxide-dark"
-                : "/tinymce/skins/ui/oxide",
-            content_css:
-              mode() === "dark"
-                ? "/tinymce/skins/content/dark/content.min.css"
-                : "/tinymce/skins/content/default/content.min.css",
-            promotion: false,
-            plugins: "lists, quickbars",
-            quickbars_image_toolbar: false,
-            quickbars_insert_toolbar: false,
-            toolbar: [
-              "undo redo | bold italic |alignleft aligncenter alignright",
-              "styles bullist numlist outdent indent",
-            ],
-            toolbar_mode: "wrap",
-            statusbar: false,
-            setup: function (editor) {
-              editor.on("change", function () {
-                tinymce.triggerSave();
-              });
-            },
-          });
-        };
-        document.body.appendChild(quickBarsPlugin);
-      };
-      document.body.appendChild(listPlugin);
-    };
-    document.body.appendChild(script);
-  });
-
+  // createEffect(() => {
+  //   window.addEventListener("storage", (event) => {
+  //     if (event.key === "theme") {
+  //       //@ts-ignore
+  //       setMode(event.newValue);
+  //     }
+  //     window.location.reload();
+  //     console.log(event.key);
+  //     console.log("Mode: " + mode());
+  //   });
+  // });
+  //
+  // createEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "/tinymce/tinymce.min.js";
+  //   script.async = true;
+  //   script.onload = () => {
+  //     const listPlugin = document.createElement("script");
+  //     listPlugin.src = "/tinymce/plugins/lists/plugin.min.js";
+  //     listPlugin.async = true;
+  //     listPlugin.onload = () => {
+  //       const quickBarsPlugin = document.createElement("script");
+  //       quickBarsPlugin.src = "/tinymce/plugins/quickbars/plugin.min.js";
+  //       quickBarsPlugin.async = true;
+  //       quickBarsPlugin.onload = () => {
+  //         console.log("tinymce loaded");
+  //         tinymce.init({
+  //           selector: "#Content",
+  //           max_width: 384,
+  //           skin_url:
+  //             mode() === "dark"
+  //               ? "/tinymce/skins/ui/oxide-dark"
+  //               : "/tinymce/skins/ui/oxide",
+  //           content_css:
+  //             mode() === "dark"
+  //               ? "/tinymce/skins/content/dark/content.min.css"
+  //               : "/tinymce/skins/content/default/content.min.css",
+  //           promotion: false,
+  //           plugins: "lists, quickbars",
+  //           quickbars_image_toolbar: false,
+  //           quickbars_insert_toolbar: false,
+  //           toolbar: [
+  //             "undo redo | bold italic |alignleft aligncenter alignright",
+  //             "styles bullist numlist outdent indent",
+  //           ],
+  //           toolbar_mode: "wrap",
+  //           statusbar: false,
+  //           setup: function (editor) {
+  //             editor.on("change", function () {
+  //               tinymce.triggerSave();
+  //             });
+  //           },
+  //         });
+  //       };
+  //       document.body.appendChild(quickBarsPlugin);
+  //     };
+  //     document.body.appendChild(listPlugin);
+  //   };
+  //   document.body.appendChild(script);
+  // });
+  //
   createEffect(async () => {
     const { data, error } = await supabase.auth.getSession();
     setSession(data.session);
@@ -319,6 +320,7 @@ export const CreateNewPost: Component = () => {
     setFormData(formData);
   }
 
+TinyMCE()
   return (
     <div>
       <form onSubmit={submit}>
